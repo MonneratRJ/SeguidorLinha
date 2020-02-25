bce780f1bd2819b5876470713e648410c71da133
Marcelo Monnerat Castello <48702825+MonneratRJ@users.noreply.github.com>
2020-02-24 22:33:42 -0300

    Update DebugMotores.ino
    Changed the pins to named vars since some pins are different in Uno and Nano

--- DebugMotores/DebugMotores.ino
+++ DebugMotores/DebugMotores.ino
@@ -1,28 +1,30 @@
+const uint8_t L1 = 9, L2 = 10, R1 = 13, R2 = 14;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(R1, OUTPUT);
+  pinMode(R2, OUTPUT);
   
   //Serial.begin(9600); //Enable Serial Communications
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(10, 0);
-  analogWrite(5, 255);
+  analogWrite(R2, 0);
+  analogWrite(L1, 255);
   delay(500);
 
-  analogWrite(5, 0);
-  analogWrite(9, 255);
+  analogWrite(L1, 0);
+  analogWrite(R1, 255);
   delay(500);
 
-  analogWrite(9, 0);
-  analogWrite(6, 255);
+  analogWrite(R1, 0);
+  analogWrite(L2, 255);
   delay(500);
 
-  analogWrite(6, 0);
-  analogWrite(10, 255);
+  analogWrite(L2, 0);
+  analogWrite(R2, 255);
   delay(500);
 }
