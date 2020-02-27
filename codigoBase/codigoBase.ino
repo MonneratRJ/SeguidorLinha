41d111227dddd8dd9781f2e35bc8849fd403310b
Marcelo Monnerat Castello <48702825+MonneratRJ@users.noreply.github.com>
2020-02-27 17:15:11 -0300

    Update codigoBase.ino

--- codigoBase/codigoBase.ino
+++ codigoBase/codigoBase.ino
@@ -1,6 +1,6 @@
 #include <QTRSensors.h>
 
-#define Kp 0 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
+#define Kp 0.3 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
 #define Kd 0 // experiment to determine this, slowly increase the speeds and adjust this value. (Note: Kp < Kd)
 
 // MaxSpeeds should be tuned if the robot seems to turn faster with one motor than with the other...
@@ -8,26 +8,26 @@
 #define leftMaxSpeed 255 // max speed of the robot
 
 // BaseSpeeds should be tuned if the robot seems to be "turning" while it sould be going straight... 
-#define rightBaseSpeed 150 // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define leftBaseSpeed 150  // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define rightBaseSpeed 200 // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define leftBaseSpeed 200  // this is the speed at which the motors should spin when the robot is perfectly on the line
 
 QTRSensors qtr;
 
-const uint8_t L1 = 5, L2 = 6, R1 = 9, R2 = 10;
+const uint8_t L1 = 6, L2 = 5, R1 = 10, R2 = 9;
 const uint8_t SensorCount = 6;
 const uint8_t EmitterPin = A6;
 uint16_t sensorValues[SensorCount];
-uint8_t position, error, lastError, motorSpeed;
+int position, error, lastError, motorSpeed;
 
 void setup()
 {
   // Setting A6 as an OUTPUT for the LEDON switch wich will be HIGH the entire run.
-  pinMode(A6, OUTPUT);
+  //pinMode(A6, OUTPUT);
   
   // configure the sensors
   qtr.setTypeRC();
   qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, SensorCount);
-  qtr.setEmitterPin(EmitterPin);
+  //qtr.setEmitterPin(EmitterPin);
 
   delay(500);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -41,6 +41,24 @@ void setup()
   }
   digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
   delay(500);
+
+  // print the calibration minimum values measured when emitters were on
+  Serial.begin(9600);
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // print the calibration maximum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
 }
 
 void loop()
@@ -49,6 +67,7 @@ void loop()
   // from 0 to 5000 (for a white line, use readLineWhite() instead)
   // uint16_t position = qtr.readLineBlack(sensorValues);
   position = qtr.readLineWhite(sensorValues);
+  
   error = position - 2500;
   
   motorSpeed = Kp * error + Kd * (error - lastError);
@@ -65,10 +84,22 @@ void loop()
   if (rightMotorSpeed < 0) rightMotorSpeed = 0;
   if (leftMotorSpeed < 0) leftMotorSpeed = 0;
 
+  // Debug
+  // Serial.print("Posicao: ");
+  // Serial.println(position);
+  // Serial.print("ERRO: ");
+  // Serial.println(error);
+  // Serial.print("Esquerda: ");
+  // Serial.println(leftMotorSpeed);
+  // Serial.print("Direita: ");
+  // Serial.println(rightMotorSpeed);
+  // Serial.println();Serial.println();
+  // delay(250);
 
   // Sends the power to the wheels
-  digitalWrite(R1, LOW);
-  analogWrite(R2, rightMotorSpeed);
-  digitalWrite(L1, LOW);
-  analogWrite(L2, leftMotorSpeed);
+  analogWrite(R1, rightMotorSpeed);
+  digitalWrite(R2, LOW);
+  analogWrite(L1, leftMotorSpeed);
+  digitalWrite(L2, LOW);
+  Serial.println(position);
 }
